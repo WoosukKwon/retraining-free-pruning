0e2a3f25fff0e05b670927d75b3cb0a8cb4a03c2
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2021-11-11 00:58:01 +0000

    Fix search algorithms

--- search.py
+++ search.py
@@ -1,5 +1,4 @@
 import argparse
-import copy
 import logging
 import os
 
@@ -14,6 +13,11 @@ from models.bert.config import BertConfig
 from models.bert.model import BertForSequenceClassification
 from tools.glue import glue_dataloader, max_seq_length, glue_dataset
 from tools.mac import compute_mac
+from tools.partition import partition_dataset
+from search.algo.evolution import EvolutionFinder
+from search.algo.random import RandomFinder
+from search.predictor.accuracy import SampleAccuracyPredictor
+from search.predictor.efficiency import MACPredictor
 
 
 logger = logging.getLogger(__name__)
@@ -33,211 +37,29 @@ parser.add_argument("--task_name", type=str, required=True, choices=[
     "qqp",
 ])
 parser.add_argument("--ckpt_dir", type=str, required=True)
-parser.add_argument("--sample_size", type=int, default=256)
-parser.add_argument("--oracle", action="store_true")
+parser.add_argument("--sample_ratio", type=float, default=0.1)
 parser.add_argument("--search_algo", required=True, choices=[
-    "greedy",
-    "beam",
     "random",
+    "evolution",
 ])
-parser.add_argument("--rand_num_iter", type=int, default=100)
-parser.add_argument("--max_acc_drop", type=float, default=1.0)
+parser.add_argument("--dataset", required=True, choices=["train", "dev"])
+parser.add_argument("--num_iter", type=int, default=100)
+parser.add_argument("--mac_threshold", type=float, default=0.7)
 parser.add_argument("--gpu", type=int, default=0)
 parser.add_argument("--tokenizer", type=str, default= None)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--log_dir", type=str, default=None)
 
 
-def sample_data(task_name, tokenizer, batch_size):
-    max_seq_len = max_seq_length(task_name)
-    dataset = glue_dataset(
-        task_name,
-        tokenizer,
-        training=False,
-        max_seq_len=max_seq_len,
-        pad_to_max=True,
-    )
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        collate_fn=default_data_collator,
-    )
-
-    sample_batch = next(iter(dataloader))
-    del dataloader
-    return sample_batch
-
-
-@torch.no_grad()
-def evaluate(model, sample_batch, head_masks, filter_masks, metric):
-    model.eval()
-    outputs = model(
-        input_ids=sample_batch["input_ids"],
-        attention_mask=sample_batch["attention_mask"],
-        head_masks=head_masks,
-        filter_masks=filter_masks,
-    )
-    if model.problem_type == "regression":
-        predictions = outputs.logits.squeeze()
-    else:
-        predictions = outputs.logits.argmax(dim=-1)
-    metric.add_batch(
-        predictions=predictions,
-        references=sample_batch["labels"],
-    )
-    eval_metric = metric.compute()
-    accuracy = eval_metric["accuracy"] # FIXME
-    return accuracy * 100.0
-
-
-@torch.no_grad()
-def test(model, head_masks, filter_masks, test_dataloader, metric, disable_tqdm=False):
-    for batch in tqdm(test_dataloader, disable=disable_tqdm):
-        outputs = model(
-            input_ids=batch["input_ids"].cuda(),
-            attention_mask=batch["attention_mask"].cuda(),
-            head_masks=head_masks,
-            filter_masks=filter_masks,
-        )
-        if model.problem_type == "regression":
-            predictions = outputs.logits.squeeze()
-        else:
-            predictions = outputs.logits.argmax(dim=-1)
-        metric.add_batch(
-            predictions=predictions,
-            references=batch["labels"],
-        )
-    eval_metric = metric.compute()
-    accuracy = eval_metric["accuracy"] # FIXME
-    return accuracy * 100.0
-
-
-@torch.no_grad()
-def config_to_masks(mask_config, max_num):
-    seq = torch.arange(1, max_num + 1)
-    masks = [(seq <= num).float().cuda() for num in mask_config]
-    return masks
-
-
-def config_to_mac(model_config, head_config, filter_config, avg_seq_len):
-    filter_group_size = int(model_config.intermediate_size / model_config.num_attention_heads) # Note: num_groups == num_attention_heads
-    mac = compute_mac(
-        head_config,
-        [num_filters * filter_group_size for num_filters in filter_config],
-        avg_seq_len,
-        model_config.hidden_size,
-        model_config.attention_head_size,
-    )
-    return mac
-
-
-def greedy_search(model, model_config, sample_batch, metric, avg_seq_len, max_acc_drop, oracle=False, test_dataloader=None):
-    num_hidden_layers = model_config.num_hidden_layers
-    num_attention_heads = model_config.num_attention_heads
-    num_filter_groups = model_config.num_attention_heads
-
-    head_config = [num_attention_heads] * num_hidden_layers
-    filter_config = [num_filter_groups] * num_hidden_layers
-
-    head_masks = config_to_masks(head_config, num_attention_heads)
-    filter_masks = config_to_masks(filter_config, num_filter_groups)
-
-    if oracle:
-        base_acc = test(model, head_masks, filter_masks, test_dataloader, metric, disable_tqdm=True)
-    else:
-        base_acc = evaluate(model, sample_batch, head_masks, filter_masks, metric)
-    acc_threshold = base_acc - max_acc_drop
-    base_mac = config_to_mac(model_config, head_config, filter_config, avg_seq_len)
-    logger.info(f"Base accuracy: {base_acc:.2f}, Accuracy threshold: {acc_threshold:.2f}")
-
-    curr_acc = base_acc
-    for i in range(2 * num_hidden_layers):
-        # Start from the last layer
-        layer_idx = 11 - int(i / 2)
-        if i % 2 == 0:
-            # FFN
-            head_masks = config_to_masks(head_config, num_attention_heads)
-            for num_groups in range(num_filter_groups):
-                tmp_filter_config = copy.deepcopy(filter_config)
-                tmp_filter_config[layer_idx] = num_groups
-                tmp_filter_masks = config_to_masks(tmp_filter_config, num_filter_groups)
-
-                if oracle:
-                    acc = test(model, head_masks, tmp_filter_masks, test_dataloader, metric, disable_tqdm=True)
-                else:
-                    acc = evaluate(model, sample_batch, head_masks, tmp_filter_masks, metric)
-                if acc >= acc_threshold:
-                    filter_config[layer_idx] = num_groups
-                    curr_acc = acc
-                    break
-            logger.info(f"Iteration {i}: Layer {layer_idx} - NUM FILTER GROUPS: {filter_config[layer_idx]} Acc: {curr_acc:.2f}")
-        else:
-            # MHA
-            filter_masks = config_to_masks(filter_config, num_filter_groups)
-            for num_heads in range(num_attention_heads):
-                tmp_head_config = copy.deepcopy(head_config)
-                tmp_head_config[layer_idx] = num_heads
-                tmp_head_masks = config_to_masks(tmp_head_config, num_attention_heads)
-
-                if oracle:
-                    acc = test(model, tmp_head_masks, filter_masks, test_dataloader, metric, disable_tqdm=True)
-                else:
-                    acc = evaluate(model, sample_batch, tmp_head_masks, filter_masks, metric)
-                if acc >= acc_threshold:
-                    head_config[layer_idx] = num_heads
-                    curr_acc = acc
-                    break
-            logger.info(f"Iteration {i}: Layer {layer_idx} - NUM HEADS: {head_config[layer_idx]} Acc: {curr_acc:.2f}")
-
-    mac = config_to_mac(model_config, head_config, filter_config, avg_seq_len)
-    reduced_ratio = mac / base_mac
-    logger.info(f"Original MAC: {base_mac / 1000000:.2f} M, Reduced MAC: {mac / 1000000:.2f} ({reduced_ratio * 100.0:.2f} %)")
-
-    return head_config, filter_config
-
-
-def random_search(num_iter, model, model_config, sample_batch, metric, avg_seq_len, max_acc_drop):
-    num_hidden_layers = model_config.num_hidden_layers
-    num_attention_heads = model_config.num_attention_heads
-    num_filter_groups = model_config.num_attention_heads
-
-    head_config = [num_attention_heads] * num_hidden_layers
-    filter_config = [num_filter_groups] * num_hidden_layers
-
-    head_masks = config_to_masks(head_config, num_attention_heads)
-    filter_masks = config_to_masks(filter_config, num_filter_groups)
-
-    base_acc = evaluate(model, sample_batch, head_masks, filter_masks, metric)
-    acc_threshold = base_acc - max_acc_drop
-    base_mac = config_to_mac(model_config, head_config, filter_config, avg_seq_len)
-    logger.info(f"Base accuracy: {base_acc:.2f}, Accuracy threshold: {acc_threshold:.2f}")
-
-    best_mac = base_mac
-    best_head_config = head_config
-    best_filter_config = filter_config
-    for i in range(num_iter):
-        head_config = np.random.randint(low=6, high=num_attention_heads + 1, size=num_hidden_layers)
-        head_config = list(head_config)
-        filter_config = np.random.randint(low=6, high=num_filter_groups, size=num_hidden_layers)
-        filter_config = list(filter_config)
-
-        head_masks = config_to_masks(head_config, max_num=num_attention_heads)
-        filter_masks = config_to_masks(filter_config, max_num=num_filter_groups)
-        acc = evaluate(model, sample_batch, head_masks, filter_masks, metric)
-
-        if acc >= acc_threshold:
-            mac = config_to_mac(model_config, head_config, filter_config, avg_seq_len)
-            if mac < best_mac:
-                best_mac = mac
-                best_head_config = head_config
-                best_filter_config = filter_config
-                logger.info(f"Iteration {i}: Heads - {head_config} Filters - {filter_config} Acc: {acc:.2f}")
-    
-    reduced_ratio = best_mac / base_mac
-    logger.info(f"Original MAC: {base_mac / 1000000:.2f} M, Reduced MAC: {best_mac / 1000000:.2f} ({reduced_ratio * 100.0:.2f} %)")
-
-    return best_head_config, best_filter_config
+def get_seq_len(dataloader):
+    val = 0.0
+    cnt = 0
+    for batch in dataloader:
+        seq_len = batch["attention_mask"].sum()
+        val += seq_len.item()
+        cnt += batch["attention_mask"].shape[0]
+    avg_seq_len = val / cnt
+    return avg_seq_len
 
 
 def main():
@@ -250,8 +72,10 @@ def main():
             args.model_name,
             args.task_name,
             args.search_algo,
-            f"acc_drop_{args.max_acc_drop}",
-            f"sample_{args.sample_size}" if not args.oracle else "oracle",
+            f"mac_{args.mac_threshold}",
+            args.dataset,
+            f"sample_{args.sample_ratio}",
+            f"num_iter_{args.num_iter}",
             f"seed_{args.seed}",
         )
     os.makedirs(args.log_dir, exist_ok=True)
@@ -273,58 +97,98 @@ def main():
     
     config = MODELS[args.model_name][0].from_pretrained(args.ckpt_dir)
     model = MODELS[args.model_name][1].from_pretrained(args.ckpt_dir, config=config)
+    model = model.cuda()
     tokenizer = AutoTokenizer.from_pretrained(
         args.tokenizer,
         use_fast=True,
         use_auth_token=None,
     )
-
-    sample_batch = sample_data(args.task_name, tokenizer, args.sample_size)
-    attention_mask = sample_batch["attention_mask"]
-    avg_seq_len = attention_mask.sum(dim=1).float().mean().item()
     metric = load_metric("glue", args.task_name)
-
-    model = model.cuda()
-    sample_batch["input_ids"] = sample_batch["input_ids"].cuda()
-    sample_batch["attention_mask"] = sample_batch["attention_mask"].cuda()
-    sample_batch["labels"] = sample_batch["labels"].cuda()
 
     test_dataloader = glue_dataloader(
         args.task_name,
-        tokenizer=tokenizer,
-        training=False,
-        batch_size=128,
+        tokenizer,
+        traing=False,
+        batch_size=256,
     )
 
-    if args.search_algo == "greedy":
-        head_config, filter_config = greedy_search(
-            model,
-            config,
-            sample_batch,
-            metric,
-            avg_seq_len,
-            max_acc_drop=args.max_acc_drop,
-            oracle=args.oracle,
-            test_dataloader=test_dataloader,
+    if args.dataset == "train":
+        sample_dataset = glue_dataset(
+            args.task_name,
+            tokenizer,
+            training=True,
+            max_seq_len=max_seq_length(args.task_name),
         )
+    else:
+        sample_dataset = glue_dataset(
+            args.task_name,
+            tokenizer,
+            training=False,
+            max_seq_len=max_seq_length(args.task_name),
+        )
+
+    if args.sample_ratio == 1.0:
+        sample_dataloader = DataLoader(
+            sample_dataset,
+            batch_size=256,
+            collate_fn=default_data_collator,
+            pin_memory=True,
+        )
+    else:
+        ratios = [1 - args.sample_ratio, args.sample_ratio]
+        others_sampler, sample_sampler = partition_dataset(sample_dataset, ratios)
+        sample_dataloader = DataLoader(
+            sample_dataset,
+            sampler=sample_sampler,
+            batch_size=256,
+            collate_fn=default_data_collator,
+            pin_memory=True,
+        )
+        if args.dataset == "dev":
+            test_dataloader = DataLoader(
+                sample_dataset,
+                sampler=others_sampler,
+                batch_size=256,
+                collate_fn=default_data_collator,
+                pin_memory=True,
+            )
+
+    avg_seq_len = get_seq_len(sample_dataloader)
+    logger.info(f"Average sequence length: {avg_seq_len}")
+
+    acc_predictor = SampleAccuracyPredictor(model, sample_dataloader, metric)
+    mac_predictor = MACPredictor(config, avg_seq_len)
+
+    if args.search_algo == "evolution":
+        finder = EvolutionFinder(config, acc_predictor, mac_predictor)
     elif args.search_algo == "random":
-        head_config, filter_config = random_search(
-            args.rand_num_iter,
-            model,
-            config,
-            sample_batch,
-            metric,
-            avg_seq_len,
-            max_acc_drop=args.max_acc_drop,
-        )
+        finder = RandomFinder(config, acc_predictor, mac_predictor)
+    head_masks, filter_masks = finder.search(args.num_iter, args.mac_threshold)
 
-    if not args.oracle:
-        head_masks = config_to_masks(head_config, config.num_attention_heads)
-        filter_masks = config_to_masks(filter_config, config.num_attention_heads)
-        acc = test(model, head_masks, filter_masks, test_dataloader, metric)
-        logger.info(f"Test accuracy: {acc:.2f}")
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(test_dataloader):
+            for k, v in batch.items():
+                batch[k] = v.to("cuda", non_blocking=True)
 
-    logger.info(f"Searched architecture: Heads - {head_config} Filters - {filter_config}")
+            outputs = model(
+                input_ids=batch["input_ids"],
+                attention_mask=batch["attention_mask"],
+                head_masks=head_masks,
+                filter_masks=filter_masks,
+            )
+            if model.problem_type == "regression":
+                predictions = outputs.logits.squeeze()
+            else:
+                predictions = outputs.logits.argmax(dim=-1)
+            metric.add_batch(
+                predictions=predictions,
+                references=batch["labels"],
+            )
+    eval_metric = metric.compute()
+    accuracy = eval_metric["accuracy"] * 100.0 # FIXME
+    logger.info(f"Test accuracy: {accuracy:.2f}")
+
 
 if __name__ == "__main__":
     main()
