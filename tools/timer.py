b6c92f4f6f627f713d7010d2624f736c591c3b01
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2021-12-15 01:06:17 +0000

    Add timers

---
+++ tools/timer.py
@@ -0,0 +1,31 @@
+import time
+
+import torch
+
+
+class CPUTimer:
+    def __init__(self, timelogs):
+        self.timelogs = timelogs
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, type, value, traceback):
+        end = time.time()
+        self.timelogs.append((end - self.start) / 1000) # ms
+
+
+class GPUTimer:
+    def __init__(self, timelogs):
+        self.timelogs = timelogs
+
+    def __enter__(self):
+        self.start_event = torch.cuda.Event(enable_timing=True)
+        self.end_event = torch.cuda.Event(enable_timing=True)
+        self.start_event.record()
+
+    def __exit__(self, type, value, traceback):
+        self.end_event.record()
+        self.end_event.synchronize()
+        elapsed_time = self.start_event.elapsed_time(self.end_event)
+        self.timelogs.append(elapsed_time)
