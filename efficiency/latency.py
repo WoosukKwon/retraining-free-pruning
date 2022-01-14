01d449793c8b109df1986833fb087d88e3b74c9f
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2022-01-14 09:58:25 +0000

    Add latency-aware search

--- efficiency/latency.py
+++ efficiency/latency.py
@@ -1,17 +1,47 @@
 import numpy as np
+import torch
 
 
-def fit_piecewise_linear(lut):
-    best = {"error": 100}
+@torch.no_grad()
+def lookup_latency(lut, mask):
+    n = int(mask.sum())
+    if n == 0:
+        return 0
+    else:
+        return lut[n - 1]
 
+
+def predict_latency(mha_lut, ffn_lut, head_mask, neuron_mask):
+    num_hidden_layers = head_mask.shape[0]
+    total = 0
+    for i in range(num_hidden_layers):
+        total += lookup_latency(mha_lut, head_mask[i])
+        total += lookup_latency(ffn_lut, neuron_mask[i])
+    return total
+
+
+class PiecewiseLinearLatency:
+
+    def __init__(self, threshold=None, c=None, slope=None):
+        self.threshold = threshold
+        self.c = c
+        self.slope = slope
+
+
+def fit_latency_fn(lut):
+    lut = np.asarray(lut)
+    latency_fn = PiecewiseLinearLatency()
+
+    min_error = 10000
     for threshold in range(1, len(lut) + 1):
         c = lut[:threshold].sum() / threshold
-        x_i = lut[threshold:] - c
-        i = np.arange(1, len(x_i) + 1)
-        if len(i) == 0:
+        y = lut[threshold:] - c
+        x = np.arange(1, len(y) + 1)
+
+        if threshold == len(lut):
             slope = 0
         else:
-            slope = (i * x_i).sum() / (i * i).sum()
+            slope = (x * y).sum() / (x * x).sum()
         slope = 0 if slope < 0 else slope
 
         approximated = [c] * threshold
@@ -20,11 +50,10 @@ def fit_piecewise_linear(lut):
         approximated = np.asarray(approximated)
 
         squared_error = ((lut - approximated) * (lut - approximated)).sum()
-        if squared_error < best["error"]:
-            best["error"] = squared_error
-            best["threshold"] = threshold
-            best["c"] = c
-            best["slope"] = slope
-            best["approximated"] = approximated
+        if squared_error < min_error:
+            min_error = squared_error
+            latency_fn.threshold = threshold
+            latency_fn.c = c
+            latency_fn.slope = slope
 
-    return best
+    return latency_fn
--- prune/search.py
+++ prune/search.py
@@ -1,8 +1,23 @@
 import torch
 
 from prune.fisher import collect_mask_grads, compute_fisher_info
-from efficiency.mac import compute_mac, mac_per_head, mac_per_neuron
 from prune.rearrange import greedy_rearrange
+from efficiency.mac import compute_mac, mac_per_head, mac_per_neuron
+from efficiency.latency import predict_latency, fit_latency_fn
+
+
+def rearrange_mask(mask, grads):
+    # NOTE: temporarily convert to CPU tensors as the arithmetic intensity is very low
+    device = mask.device
+    mask = mask.cpu()
+    grads = grads.cpu()
+
+    num_hidden_layers = mask.shape[0]
+    for i in range(num_hidden_layers):
+        mask[i] = greedy_rearrange(mask[i], grads[:, i, :])
+
+    mask = mask.to(device)
+    return mask
 
 
 def search_mac(
@@ -60,16 +75,101 @@ def search_mac(
     neuron_mask = neuron_mask.view(num_hidden_layers, intermediate_size)
 
     # Rearrange the mask
-    # NOTE: temporarily convert to CPU tensors as the arithmetic intensity is very low
-    head_mask = head_mask.cpu()
-    neuron_mask = neuron_mask.cpu()
-    head_grads = head_grads.cpu()
-    neuron_grads = neuron_grads.cpu()
+    head_mask = rearrange_mask(head_mask, head_grads)
+    neuron_mask = rearrange_mask(neuron_mask, neuron_grads)
+    return head_mask, neuron_mask
 
-    for i in range(num_hidden_layers):
-        head_mask[i] = greedy_rearrange(head_mask[i], head_grads[:, i, :])
-        neuron_mask[i] = greedy_rearrange(neuron_mask[i], neuron_grads[:, i, :])
 
-    head_mask = head_mask.cuda()
-    neuron_mask = neuron_mask.cuda()
+def search_latency(
+    model,
+    config,
+    prev_head_mask,
+    prev_neuron_mask,
+    dataloader,
+    latency_constraint,
+    mha_lut,
+    ffn_lut,
+):
+    assert latency_constraint < 1
+
+    num_hidden_layers = config.num_hidden_layers
+    num_attention_heads = config.num_attention_heads
+    intermediate_size = config.intermediate_size
+
+    original_latency = predict_latency(
+        mha_lut,
+        ffn_lut,
+        torch.ones_like(prev_head_mask),
+        torch.ones_like(prev_neuron_mask),
+    )
+    max_latency = latency_constraint * original_latency
+
+    mha_latency_fn = fit_latency_fn(mha_lut)
+    ffn_latency_fn = fit_latency_fn(ffn_lut)
+
+    head_grads, neuron_grads = collect_mask_grads(model, prev_head_mask, prev_neuron_mask, dataloader)
+    head_importance = compute_fisher_info(head_grads)
+    neuron_importance = compute_fisher_info(neuron_grads)
+
+    # Must include at least one head/neuron per layer
+    max_latency = max_latency - num_hidden_layers * mha_latency_fn.c
+    max_latency = max_latency - num_hidden_layers * ffn_latency_fn.c
+    assert max_latency > 0
+
+    # Locally rank heads and neurons
+    _, local_head_indicies = head_importance.sort(dim=1, descending=True)
+    _, local_neuron_indicies = neuron_importance.sort(dim=1, descending=True)
+    base_head_indicies = local_head_indicies[:, :mha_latency_fn.threshold]
+    base_neuron_indicies = local_neuron_indicies[:, :ffn_latency_fn.threshold]
+
+    remaining_head_indicies = local_head_indicies[:, mha_latency_fn.threshold:]
+    remaining_neuron_indicies = local_neuron_indicies[:, ffn_latency_fn.threshold:]
+
+    remaining_head_importance = head_importance.gather(dim=1, index=remaining_head_indicies)
+    remaining_neuron_importance = neuron_importance.gather(dim=1, index=remaining_neuron_indicies)
+
+    # Globally rank the remaining heads and neurons
+    sorted_head_importance, sorted_head_indicies = remaining_head_importance.view(-1).sort(descending=True)
+    sorted_neuron_importance, sorted_neuron_indicies = remaining_neuron_importance.view(-1).sort(descending=True)
+
+    max_importance = 0
+    num_remaining_heads = num_hidden_layers * (num_attention_heads - mha_latency_fn.threshold)
+    for num_heads in range(1, num_remaining_heads + 1):
+        heads_latency = mha_latency_fn.slope * num_heads
+        neurons_latency = max_latency - heads_latency
+        num_neurons = int(neurons_latency / ffn_latency_fn.slope)
+        if num_neurons < 0:
+            continue
+
+        total_importance = sorted_head_importance[:num_heads].sum() + sorted_neuron_importance[:num_neurons].sum()
+        if total_importance > max_importance:
+            max_importance = total_importance
+            head_indicies = sorted_head_indicies[:num_heads]
+            neuron_indicies = sorted_neuron_indicies[:num_neurons]
+
+    offset = torch.arange(0, end=num_hidden_layers).unsqueeze(1).cuda() * num_attention_heads
+    base_head_indicies += offset
+    base_head_indicies = base_head_indicies.flatten()
+    remaining_head_indicies += offset
+    head_indicies = remaining_head_indicies.flatten()[head_indicies]
+    head_indicies = torch.cat([base_head_indicies, head_indicies], dim=0)
+
+    offset = torch.arange(0, end=num_hidden_layers).unsqueeze(1).cuda() * intermediate_size
+    base_neuron_indicies += offset
+    base_neuron_indicies = base_neuron_indicies.flatten()
+    remaining_neuron_indicies += offset
+    neuron_indicies = remaining_neuron_indicies.flatten()[neuron_indicies]
+    neuron_indicies = torch.cat([base_neuron_indicies, neuron_indicies], dim=0)
+
+    head_mask = torch.zeros_like(prev_head_mask).flatten()
+    head_mask[head_indicies] = 1.0
+    head_mask = head_mask.view(num_hidden_layers, num_attention_heads)
+
+    neuron_mask = torch.zeros_like(prev_neuron_mask).flatten()
+    neuron_mask[neuron_indicies] = 1.0
+    neuron_mask = neuron_mask.view(num_hidden_layers, intermediate_size)
+
+    # Rearrange the mask
+    head_mask = rearrange_mask(head_mask, head_grads)
+    neuron_mask = rearrange_mask(neuron_mask, neuron_grads)
     return head_mask, neuron_mask
